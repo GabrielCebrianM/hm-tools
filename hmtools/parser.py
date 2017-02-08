aee2d6c3f93f223c407377ffda6775bafaa3440b
Gabriel Cebrián <GabrielCebrianM@users.noreply.github.com>
2017-02-08 13:12:25 +0100

    Fixed encoding problem with the Spanish accent in license headings

--- hmtools/parser.py
+++ hmtools/parser.py
@@ -1,5 +1,5 @@
 #
-#   Copyright 2017 Gabriel Cebrián
+#   Copyright 2017 Gabriel Cebrian
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
